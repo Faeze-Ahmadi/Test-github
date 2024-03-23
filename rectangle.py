c411a2ac391a2a19e9c830ba153e1c1ca0a0943a
Faeze <faezeahmadi7173@gmail.com>
2024-03-23 13:48:50 +0330

    copy the same code of def-isPrime

--- rectangle.py
+++ rectangle.py
@@ -22,3 +22,13 @@ def is_prime(n):
 print(is_prime(101))
 
 print("---------------------------------------")
+
+def is_prime(n):
+    if n == 1: 
+        return False
+    if n == 2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False 
+    return True
