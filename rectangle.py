7c0d0f7b022a7cbce25805773ffa6065135e98a4
Faeze <faezeahmadi7173@gmail.com>
2024-03-23 15:41:03 +0330

    end of calculating

--- rectangle.py
+++ rectangle.py
@@ -53,4 +53,5 @@ def grades(my_list: list)-> str:
     minimum = min(my_list)
     maximum = max(my_list)
     mean = round(sum(my_list) / len(my_list), 2)
+    return f'Minimum: {minimum}\n Maximum: {maximum}\n Mean:{mean}'
     
